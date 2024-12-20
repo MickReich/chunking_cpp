2717d950bf0fbc86780917b9786c254dc19b58f7
JohnnyTeutonic <jonathan@DESKTOP-2S63ISE.>
2024-12-21 01:18:38 +1100

    remove redundant cmake-generated files

--- CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/jonat/OneDrive/Documents/Python Scripts/chunking_cpp
-# Build directory: /mnt/c/Users/jonat/OneDrive/Documents/Python Scripts/chunking_cpp
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(AllTests "/mnt/c/Users/jonat/OneDrive/Documents/Python Scripts/chunking_cpp/tests/run_tests")
-set_tests_properties(AllTests PROPERTIES  _BACKTRACE_TRIPLES "/mnt/c/Users/jonat/OneDrive/Documents/Python Scripts/chunking_cpp/CMakeLists.txt;104;add_test;/mnt/c/Users/jonat/OneDrive/Documents/Python Scripts/chunking_cpp/CMakeLists.txt;0;")
